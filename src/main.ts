1ae98ec9b96cbbbddf175a73b25419f127636feb
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-12-04 00:36:07 +0900

    handlebars 대신 ejs로 템플릿 엔진 교체

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as path from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.setViewEngine('hbs');
+  app.useStaticAssets(path.join(__dirname, '..', 'public'));
+  app.setViewEngine('ejs');
   app.setBaseViewsDir(path.join(__dirname, '..', 'views'));
 
   await app.listen(process.env.PORT ?? 3000);
