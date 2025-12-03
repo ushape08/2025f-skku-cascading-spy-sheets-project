e1cfa4ee47a84c5b8cd0d5935213684c37edc07f
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-12-04 01:18:03 +0900

    무작위 16자리 문자열 생성하는 함수 구현

--- src/tracking/tracking.service.ts
+++ src/tracking/tracking.service.ts
@@ -1,12 +1,22 @@
 import { Injectable, Res } from '@nestjs/common';
 import path from 'path';
 import * as fs from 'fs';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class TrackingService {
+  private readonly ID_LENGTH = 16;
+
   getImage(imagePath: string): Buffer<ArrayBuffer> {
     const realImagePath = path.join(process.cwd(), imagePath);
     const image = fs.readFileSync(realImagePath);
     return image;
+  }
+
+  generateRandomId(): string {
+    return crypto
+      .randomBytes(Math.ceil(this.ID_LENGTH / 2))
+      .toString('hex')
+      .slice(0, this.ID_LENGTH);
   }
 }
