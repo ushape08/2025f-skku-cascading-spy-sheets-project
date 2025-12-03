2403911883aaa9f6088da9da0552852bb67db738
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-12-03 20:54:04 +0900

    handlebars example 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setViewEngine('hbs');
+  app.setBaseViewsDir(path.join(__dirname, '..', 'views'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
