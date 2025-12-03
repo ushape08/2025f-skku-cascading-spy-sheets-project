e6bab203d6c9dc9da8ae56448d5de5b5552093e3
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-12-03 21:58:17 +0900

    vercel 환경에서 hbs를 찾을 수 없는 문제 해결

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as path from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
