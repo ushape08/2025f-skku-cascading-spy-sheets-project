ba790a05dd5fa6e4791482fa8234eda92f6adbc6
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-12-04 16:47:02 +0900

    configuration 표기법 수정

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,6 +1,6 @@
 export default () => ({
-  port: parseInt(process.env.PORT || '3000', 10),
-  serverDomain: process.env.SERVER_DOMAIN ?? 'http://localhost:3000',
-  mongoDBUri:
+  PORT: parseInt(process.env.PORT || '3000', 10),
+  SERVER_DOMAIN: process.env.SERVER_DOMAIN ?? 'http://localhost:3000',
+  MONGODB_URI:
     process.env.MONGODB_URI || 'mongodb://localhost/cascading-spy-sheets',
 });
