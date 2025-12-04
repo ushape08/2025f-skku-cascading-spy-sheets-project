b9cefff8147084ec47935c725e296b7687ac16a4
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-12-04 16:25:47 +0900

    MongoDB 사용을 위한 @nestjs/mongoose 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrackingModule } from './tracking/tracking.module';
 import { PageModule } from './page/page.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TrackingModule, PageModule],
+  imports: [
+    TrackingModule,
+    PageModule,
+    MongooseModule.forRoot(process.env.MONGODB_URL ?? ''),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
